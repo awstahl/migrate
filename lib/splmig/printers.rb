2ead34fdc7c28d9d80c88f647fd77fd736bfd31b
awstahl <alex@testcore.net>
2016-08-30 14:27:22 -0700

    First pass at a couple printers

--- lib/splmig/printers.rb
+++ lib/splmig/printers.rb
@@ -1,8 +1,40 @@
+#
+#  File: printers.rb
+#  Author: alex@testcore.net
+#
+# Classes to convert various data structures
+# to printable strings.
 
+require "#{ File.dirname __FILE__ }/validators"
 
 module Migration
 
-  class Printers
+  class IniPrinter
+
+    class << self
+
+      def print(header, data)
+        out = "[#{ header }]\n"
+        Valid.hash? data do
+          data.keys.sort.each do |key|
+            out += "#{ key } = #{ data[ key ]}\n"
+          end
+        end
+        out
+      end
+    end
+  end
+
+
+  class ConfPrinter
+    class << self
+
+      def print(stanzas)
+        Valid.array? stanzas do
+          stanzas.join "\n\n"
+        end
+      end
+    end
   end
 
 end
--- spec/printers_spec.rb
+++ spec/printers_spec.rb
@@ -1,9 +1,37 @@
 require 'rspec'
+require "#{ File.dirname __FILE__ }/../lib/splmig/printers.rb"
 
-describe 'Migration Printer' do
 
-  it 'should exist' do
+describe 'Migration ini stanza printing' do
 
-    true.should == false
+  it 'exists' do
+    expect( Object.const_defined? 'Migration::IniPrinter' ).to be_truthy
   end
+
+  it 'prints an ini stanza from a header string and hash' do
+    expect( Migration::IniPrinter.print 'test ini stanza', enabled: true, zeta: 'maybe', queue: false ).to \
+    eq( "[test ini stanza]\nenabled = true\nqueue = false\nzeta = maybe\n" )
+  end
+
+  it 'requires a valid hash' do
+    expect( Migration::IniPrinter.print 3.14, 'not a hash' ).to eq( "[3.14]\n")
+  end
+
+end
+
+
+describe 'Migration conf file printing' do
+
+  it 'exists' do
+    expect( Object.const_defined? 'Migration::ConfPrinter' ).to be_truthy
+  end
+
+  it 'prints a conf file' do
+    expect( Migration::ConfPrinter.print %w[ stanza block line ]).to eq( "stanza\n\nblock\n\nline" )
+  end
+
+  it 'requires an array to print' do
+    expect( Migration::ConfPrinter.print 'not an array' ).to be_falsey
+  end
+
 end
