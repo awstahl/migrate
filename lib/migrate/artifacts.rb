24c2437a36c470bc09c72743343a54cb57e127a3
awstahl <alex@testcore.net>
2017-05-16 00:40:40 -0700

    Begin breaking out Applications classes

--- lib/migrate/artifacts.rb
+++ lib/migrate/artifacts.rb
@@ -4,14 +4,15 @@
 #
 #  This is a set of classes that models config data.
 
+require "#{ File.dirname __FILE__ }/applications"
 require "#{ File.dirname __FILE__ }/exceptions"
 require "#{ File.dirname __FILE__ }/logger"
-require "#{ File.dirname __FILE__ }/server"
-require "#{ File.dirname __FILE__ }/sugar"
 require "#{ File.dirname __FILE__ }/parents"
-require "#{ File.dirname __FILE__ }/validators"
 require "#{ File.dirname __FILE__ }/parsers"
 require "#{ File.dirname __FILE__ }/printers"
+require "#{ File.dirname __FILE__ }/server"
+require "#{ File.dirname __FILE__ }/sugar"
+require "#{ File.dirname __FILE__ }/validators"
 
 
 module Migration
